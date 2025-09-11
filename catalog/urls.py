b537b8f0e64be549d5c534145ced5ca2d29b2559
Saidova <saidova.kseniya@mail.ru>
2025-09-11 23:27:40 +0300

    в config/urls.py есть обработка медиафайлов

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
 from catalog.views import home, contacts
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = CatalogConfig.name
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('', home, name = 'home'),
     path('contacts/', contacts, name='contacts'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
