6de9d55d00dea1c1c3911fb580debdb4f1c6f622
Saidova <saidova.kseniya@mail.ru>
2025-09-28 14:12:58 +0300

    Обновлен urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,11 @@
-from django.urls import path, include
+from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts
-from django.conf import settings
-from django.conf.urls.static import static
+from catalog.views import home, contacts, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name = 'home'),
+    path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
