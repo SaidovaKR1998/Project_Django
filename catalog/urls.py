ae5393803abb4e540ad34392462cb15effed2c20
Saidova <saidova.kseniya@mail.ru>
2025-09-28 14:58:48 +0300

    Обновлен catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,23 @@
+#from django.urls import path
+#from catalog.apps import CatalogConfig
+#from catalog.views import home, contacts, product_detail
+
+#app_name = CatalogConfig.name
+
+#urlpatterns = [
+#    path('', home, name='home'),
+#    path('contacts/', contacts, name='contacts'),
+#    path('product/<int:product_id>/', product_detail, name='product_detail'),
+#]
+
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, product_detail
+from catalog.views import HomeView, ProductDetailView, ContactsView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:product_id>/', product_detail, name='product_detail'),
+    path('', HomeView.as_view(), name='home'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
