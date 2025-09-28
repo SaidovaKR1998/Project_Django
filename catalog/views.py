697f8796523d6e0a9c050b9af0e828c7126c6031
Saidova <saidova.kseniya@mail.ru>
2025-09-28 14:11:49 +0300

    Обновлен views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    return render(request, 'home.html', {'products': products})
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
