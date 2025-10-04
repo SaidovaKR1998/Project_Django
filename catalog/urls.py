06150acdc917c8cab8b8208d5ce5aff4043887d5
Saidova <saidova.kseniya@mail.ru>
2025-10-04 13:16:02 +0300

    Настраиваем URL-адреса в catalog/urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,23 +1,22 @@
-#from django.urls import path
-#from catalog.apps import CatalogConfig
-#from catalog.views import home, contacts, product_detail
-
-#app_name = CatalogConfig.name
-
-#urlpatterns = [
-#    path('', home, name='home'),
-#    path('contacts/', contacts, name='contacts'),
-#    path('product/<int:product_id>/', product_detail, name='product_detail'),
-#]
-
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import HomeView, ProductDetailView, ContactsView
+from .views import (
+    HomeView, ContactsView, ProductListView,
+    ProductDetailView, ProductCreateView,
+    ProductUpdateView, ProductDeleteView
+)
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
+    # Основные страницы
     path('', HomeView.as_view(), name='home'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
+
+    # CRUD для продуктов
+    path('products/', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
