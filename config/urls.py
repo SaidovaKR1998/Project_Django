9394fd346eaf764e73386abb106e0027be8aa74b
Saidova <saidova.kseniya@mail.ru>
2025-10-04 13:52:30 +0300

    Подключаем URLs в главный urls.py Обновляем config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
-    path('blog/', include('blog.urls', namespace='blog')),  # Добавьте эту строку
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('users/', include('users.urls', namespace='users')),  # ← ДОБАВЛЯЕМ ЭТУ СТРОКУ
 ]
+
+# Добавляем обработку медиа-файлов в режиме разработки
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
